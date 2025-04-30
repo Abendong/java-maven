d564ec15901c9d8b53a6addc4ce3a269f270aa75
Akoanung Ayaba Abendong <94168772+Abendong@users.noreply.github.com>
2025-04-30 19:13:04 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t abendong/my-app:jma-2.0 .'
+        sh 'docker build -t abendong/jenkins-ayaba-demo:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push abendong/my-app:jma-2.0'
+        sh 'docker push abendong/jenkins-ayaba-demo:jma-2.0'
     }
 } 
 
